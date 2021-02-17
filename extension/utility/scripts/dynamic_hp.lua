bfef4fe52f414d83f65decb0d8acfa6605ad7882
Shane Parker <shane.tim.parker@gmail.com>
2021-02-17 19:46:22 +0000

    Fixed casing on Session.IsHost, updated version, and apackaged extension.

--- extension/utility/scripts/dynamic_hp.lua
+++ extension/utility/scripts/dynamic_hp.lua
@@ -39,7 +39,7 @@ function onClose()
 end
 
 function onDrop(x, y, draginfo)
-	if Session.isHost then
+	if Session.IsHost then
 		if draginfo.getType() == "number" then
 			local node = window.getDatabaseNode();
 			local rActor = ActorManager.getActorFromCT(node);
