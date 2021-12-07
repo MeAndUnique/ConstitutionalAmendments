b9dbd0e0afeeb85e4dd16815b0eacc0d17c2d7cf
Shane Parker <shane.tim.parker@ea.com>
2021-12-07 12:16:29 -0500

    Added option to hide extra health fields.

--- campaign/scripts/npc_main.lua
+++ campaign/scripts/npc_main.lua
@@ -4,7 +4,25 @@
 --
 
 function onInit()
-	if not shouldHandleExtraHealthFields() then
+	if canHandleExtraHealthFields() then
+		onHealthChanged();
+		OptionsManager.registerCallback("WNDC", onHealthChanged);
+		changeHealthDisplay();
+		OptionsManager.registerCallback("HPDM", changeHealthDisplay);
+		showOrHideHealthFields();
+		OptionsManager.registerCallback("NPCHF", showOrHideHealthFields);
+
+		if ColorManager.COLOR_TEMP_HP then
+			hptemp.setColor(ColorManager.COLOR_TEMP_HP);
+		end
+		if ColorManager.COLOR_ADJUSTED_HP then
+			hpadjust.setColor(ColorManager.COLOR_ADJUSTED_HP);
+		end
+
+		if DB.getChildCount(getDatabaseNode(), "classes") == 0 then
+			HpManager.updateNpcHitDice(getDatabaseNode());
+		end
+	else
 		wounds.setVisible(false);
 		wounds_label.setVisible(false);
 		hptemp_label.setVisible(false);
@@ -21,22 +39,6 @@ function onInit()
 		deathsavesuccess_label.setVisible(false);
 		deathsavefail.setVisible(false);
 		deathsavefail_label.setVisible(false);
-	else
-		onHealthChanged();
-		OptionsManager.registerCallback("WNDC", onHealthChanged);
-		changeHealthDisplay();
-		OptionsManager.registerCallback("HPDM", changeHealthDisplay);
-
-		if ColorManager.COLOR_TEMP_HP then
-			hptemp.setColor(ColorManager.COLOR_TEMP_HP);
-		end
-		if ColorManager.COLOR_ADJUSTED_HP then
-			hpadjust.setColor(ColorManager.COLOR_ADJUSTED_HP);
-		end
-
-		if DB.getChildCount(getDatabaseNode(), "classes") == 0 then
-			HpManager.updateNpcHitDice(getDatabaseNode());
-		end
 	end
 
 	if super and super.onInit then
@@ -44,9 +46,30 @@ function onInit()
 	end
 end
 
-function shouldHandleExtraHealthFields()
+function canHandleExtraHealthFields()
 	local node = getDatabaseNode();
 	return CombatManager.getCTFromNode(node);
+end
+
+function showOrHideHealthFields()
+	bShow = OptionsManager.isOption("NPCHF", "");
+
+	wounds.setVisible(bShow);
+	wounds_label.setVisible(bShow);
+	hptemp_label.setVisible(bShow);
+	hptemp.setVisible(bShow);
+	hpadjust_label.setVisible(bShow);
+	hpadjust.setVisible(bShow);
+
+	hd_label.setVisible(bShow);
+	hitdice.setVisible(bShow);
+
+	deathsave_label.setVisible(bShow);
+	deathsave_roll.setVisible(bShow);
+	deathsavesuccess.setVisible(bShow);
+	deathsavesuccess_label.setVisible(bShow);
+	deathsavefail.setVisible(bShow);
+	deathsavefail_label.setVisible(bShow);
 end
 
 function onHealthChanged()
